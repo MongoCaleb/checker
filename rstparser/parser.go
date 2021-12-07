cf74214bb2a038e6f1e90bbc52241ebff039beae
Nathan Leniz <nathan.leniz@mongodb.com>
2021-12-06 22:58:03 -0800

    rstparser and tests

--- rstparser/parser.go
+++ rstparser/parser.go
@@ -10,29 +10,52 @@ func Parse() {
 	fmt.Println("parser")
 }
 
-type Location struct {
-	Line   int
-	Column int
+type RstRole struct {
+	Target   string
+	RoleType string
+	Name     string
 }
 
-type Ref struct {
-	Match    string
-	Location Location
+type RstConstant struct {
+	Name   string
+	Target string
 }
 
-func refParse(input []byte) []string {
-	matches := make([]string, 0)
+func roleParse(input []byte) []RstRole {
+	matches := make([]RstRole, 0)
 	re := regexp.MustCompile(`:([\w\s\-\.\d_=+!@#$%^&*(\)]*):\x60(?P<one>[\w\s\-_\.\d\\\/=+!@#$%^&*(\)]*)<?(?P<two>[\w\s\-_\.\d\\\/=+!@#$%^&*(\)]*)>?`)
 	allIndexes := re.FindAllSubmatchIndex([]byte(input), -1)
 	for _, loc := range allIndexes {
 		extract := input[loc[0]:loc[1]]
 		innerMatches := re.FindAllStringSubmatch(strings.Join(strings.Fields(string(extract)), ""), -1)
 		for _, match := range innerMatches {
-			if match[3] == "" {
-				matches = append(matches, match[2])
+			roleType, name := "", ""
+			if match[1] == "ref" {
+				roleType = "ref"
+				name = "ref"
 			} else {
-				matches = append(matches, match[3])
+				roleType = "role"
+				name = match[1]
 			}
+			if match[3] == "" {
+				matches = append(matches, RstRole{Target: match[2], RoleType: roleType, Name: name})
+			} else {
+				matches = append(matches, RstRole{Target: match[3], RoleType: roleType, Name: name})
+			}
+		}
+	}
+	return matches
+}
+
+func constantParse(input []byte) []RstConstant {
+	matches := make([]RstConstant, 0)
+	re := regexp.MustCompile(`<\{\+(.*)\+\}(\/.*)>\x60`)
+	allIndexes := re.FindAllSubmatchIndex([]byte(input), -1)
+	for _, loc := range allIndexes {
+		extract := input[loc[0]:loc[1]]
+		innerMatches := re.FindAllStringSubmatch(strings.Join(strings.Fields(string(extract)), ""), -1)
+		for _, match := range innerMatches {
+			matches = append(matches, RstConstant{Target: match[2], Name: match[1]})
 		}
 	}
 	return matches
--- rstparser/parser_test.go
+++ rstparser/parser_test.go
@@ -4,41 +4,126 @@ import (
 	"testing"
 )
 
-type testCase struct {
+type roleTestCase struct {
 	input    string
-	expected []string
+	expected []RstRole
+}
+
+type constantTestCase struct {
+	input    string
+	expected []RstConstant
 }
 
 //input:    "here is a :ref:`fantastic` here is another :ref:`2 <mediocre-fantastic>` here is a :ref:`\n<not_great-fantastic>",
 func TestRefParser(t *testing.T) {
 
-	testCases := []testCase{{
+	testCases := []roleTestCase{{
 		input:    "",
-		expected: []string{""},
+		expected: []RstRole{},
 	}, {
 		input:    ".. _:",
-		expected: []string{""},
+		expected: []RstRole{},
 	}, {
 		input:    ".. _: foo",
-		expected: []string{""},
+		expected: []RstRole{},
+	}, {
+		input:    "This is a `constant link that should fail <{+api+}/flibbertypoo>`__",
+		expected: []RstRole{},
+	}, {
+		input:    "This is a `constant link that should succeed <{+api+}/classes/AggregationCursor.html>`__",
+		expected: []RstRole{},
 	}, {
 		input:    "here is a :ref:`fantastic`",
-		expected: []string{"fantastic"},
+		expected: []RstRole{{Target: "fantastic", RoleType: "ref", Name: "ref"}},
 	}, {
 		input:    "here is a :ref:`fantastic` here is another :ref:`2 <mediocre-fantastic>` here is a :ref:`\n<not_great-fantastic>`",
-		expected: []string{"fantastic", "mediocre-fantastic", "not_great-fantastic"},
-	}}
+		expected: []RstRole{{Target: "fantastic", RoleType: "ref", Name: "ref"}, {Target: "mediocre-fantastic", RoleType: "ref", Name: "ref"}, {Target: "not_great-fantastic", RoleType: "ref", Name: "ref"}},
+	}, {
+		input:    ":node-api:`foo </AggregationCursor.html>`",
+		expected: []RstRole{{Target: "/AggregationCursor.html", RoleType: "role", Name: "node-api"}},
+	}, {
+		input:    ":node-api:`foo <AggregationCursorz.html>`",
+		expected: []RstRole{{Target: "AggregationCursorz.html", RoleType: "role", Name: "node-api"}},
+	}, {
+		input:    ":node-api:`foo <AggregationCursor.html>`",
+		expected: []RstRole{{Target: "AggregationCursor.html", RoleType: "role", Name: "node-api"}},
+	}, {
+		input:    "This is a :ref:`valid atlas ref <connect-to-your-cluster>`",
+		expected: []RstRole{{Target: "connect-to-your-cluster", RoleType: "ref", Name: "ref"}},
+	}, {
+		input:    "This is a :ref:`valid server ref <replica-set-read-preference-behavior>`",
+		expected: []RstRole{{Target: "replica-set-read-preference-behavior", RoleType: "ref", Name: "ref"}},
+	}, {
+		input:    "This is an :ref:`nvalid ref <invalid_ref_sucka-fish>`",
+		expected: []RstRole{{Target: "invalid_ref_sucka-fish", RoleType: "ref", Name: "ref"}},
+	},
+	}
 
 	for _, test := range testCases {
-		got := refParse([]byte(test.input))
+		got := roleParse([]byte(test.input))
 		for i, find := range got {
 			if len(got) != len(test.expected) {
 				t.Errorf("expected length %d, got %d with %q", len(test.expected), len(got), find)
 			}
 			if find != test.expected[i] {
-				t.Errorf("refParse(%q) == %q, want %q", test.input, got, test.expected[i])
+				t.Errorf("expected %q, got %q with %q", test.expected[i], find, test.input)
 			}
 		}
 	}
+}
 
+func TestConstantParser(t *testing.T) {
+
+	testCases := []constantTestCase{{
+		input:    "",
+		expected: []RstConstant{},
+	}, {
+		input:    ".. _:",
+		expected: []RstConstant{},
+	}, {
+		input:    ".. _: foo",
+		expected: []RstConstant{},
+	}, {
+		input:    "This is a `constant link that should fail <{+api+}/flibbertypoo>`__",
+		expected: []RstConstant{{Target: "/flibbertypoo", Name: "api"}},
+	}, {
+		input:    "This is a `constant link that should succeed <{+api+}/classes/AggregationCursor.html>`__",
+		expected: []RstConstant{{Target: "/classes/AggregationCursor.html", Name: "api"}},
+	}, {
+		input:    "here is a :ref:`fantastic`",
+		expected: []RstConstant{},
+	}, {
+		input:    "here is a :ref:`fantastic` here is another :ref:`2 <mediocre-fantastic>` here is a :ref:`\n<not_great-fantastic>`",
+		expected: []RstConstant{},
+	}, {
+		input:    ":node-api:`foo </AggregationCursor.html>`",
+		expected: []RstConstant{},
+	}, {
+		input:    ":node-api:`foo <AggregationCursorz.html>`",
+		expected: []RstConstant{},
+	}, {
+		input:    ":node-api:`foo <AggregationCursor.html>`",
+		expected: []RstConstant{},
+	}, {
+		input:    "This is a :ref:`valid atlas ref <connect-to-your-cluster>`",
+		expected: []RstConstant{},
+	}, {
+		input:    "This is a :ref:`valid server ref <replica-set-read-preference-behavior>`",
+		expected: []RstConstant{},
+	}, {
+		input:    "This is an :ref:`nvalid ref <invalid_ref_sucka-fish>`",
+		expected: []RstConstant{},
+	},
+	}
+	for _, test := range testCases {
+		got := constantParse([]byte(test.input))
+		for i, find := range got {
+			if len(got) != len(test.expected) {
+				t.Errorf("expected length %d, got %d with %q", len(test.expected), len(got), find)
+			}
+			if find != test.expected[i] {
+				t.Errorf("expected %q, got %q with %q", test.expected[i], find, test.input)
+			}
+		}
+	}
 }
